61c43361061fe4c88e271874eed46bfafa4fc644
Nicola Pancheri <nicola.pancheri@netduma.com>
2023-03-22 14:02:34 +0000

    refactoring

--- src/tcp_client.c
+++ src/tcp_client.c
@@ -12,59 +12,58 @@
 static struct sockaddr_in g_servaddr;
 
 int tcp_client_init(const char* srv_addr, int srv_port) {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (sockfd < 0) {
-		return -1;
-	}
+  if (sockfd < 0) {
+    return -1;
+  }
 
-	bzero(&g_servaddr, sizeof(g_servaddr));
+  bzero(&g_servaddr, sizeof(g_servaddr));
 
-	// assign IP, PORT
-	g_servaddr.sin_family = AF_INET;
-	g_servaddr.sin_addr.s_addr = inet_addr(srv_addr);
-	g_servaddr.sin_port = htons(srv_port);
+  // assign IP, PORT
+  g_servaddr.sin_family = AF_INET;
+  g_servaddr.sin_addr.s_addr = inet_addr(srv_addr);
+  g_servaddr.sin_port = htons(srv_port);
 
-	return sockfd;
+  return sockfd;
 }
 
 int tcp_client_connect(int sockfd) {
-	if (connect(sockfd, (struct sockaddr*)&g_servaddr,
-		    sizeof(g_servaddr)) != 0) {
-		return -1;
-	}
+  if (connect(sockfd, (struct sockaddr*)&g_servaddr, sizeof(g_servaddr)) != 0) {
+    return -1;
+  }
 
-	return 0;
+  return 0;
 }
 
 void tcp_client_cleanup(int sockfd) { close(sockfd); }
 
 int tcp_client_run(const char* srv_addr, int srv_port, int (*auth_cb)(),
-		   void (*exec_cb)(int)) {
-	int sockfd = tcp_client_init(srv_addr, srv_port);
+                   void (*exec_cb)(int)) {
+  int sockfd = tcp_client_init(srv_addr, srv_port);
 
-	if (sockfd < 0) return -1;
+  if (sockfd < 0) return -1;
 
-	if (tcp_client_connect(sockfd)) {
-		return -2;
-	}
+  if (tcp_client_connect(sockfd)) {
+    return -2;
+  }
 
-	if (auth_cb == NULL || !(*auth_cb)()) (*exec_cb)(sockfd);
+  if (auth_cb == NULL || !(*auth_cb)()) (*exec_cb)(sockfd);
 
-	tcp_client_cleanup(sockfd);
+  tcp_client_cleanup(sockfd);
 
-	return 0;
+  return 0;
 }
 
 void print_hello(int sockfd) { printf("Hello by the client\n"); }
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
-		printf("Invalid number of arguments\n");
-		return -1;
-	}
+  if (argc != 3) {
+    printf("Invalid number of arguments\n");
+    return -1;
+  }
 
-	int port_num = atoi(argv[2]);
-	return tcp_client_run(argv[1], port_num, NULL, print_hello);
+  int port_num = atoi(argv[2]);
+  return tcp_client_run(argv[1], port_num, NULL, print_hello);
 }
 
